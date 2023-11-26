3f1e4ec4087cb10b1ef1243f883dad0d6cb3483e
WAJDI RIAHI <rwajdi2020@gmail.com>
2023-11-26 13:48:16 +0100

    modifiying code

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,23 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const GET_POKEMONS = gql`
-  query getPokemons($limit: Int, $offset: Int) {
-    pokemons(limit: $limit, offset: $offset) {
-      count
-      results {
-        id
-        name
-        image
-        types {
-          type {
-            name
-          }
-        }
-      }
-    }
-  }
-`;
-
 export const SEARCH_POKEMON = gql`
   query searchPokemon($name: String!) {
     pokemon(name: $name) {
